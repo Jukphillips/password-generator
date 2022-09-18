5db0feafaad969224675c772099c2aa4740e9d69
Justin Phillips <Jukphillips1990@gmail.com>
2022-09-18 06:23:48 -0600

    connected events in html to javascript to act in passwordgenerator function when change in state occurs

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
-let passGeneration = document.getElementById('password') // textarea for password Generation
-let slider = document.getElementById('range') // slider range 
-let output = document.getElementById('sliderValue') // slider label
-
-
-
+let passGeneration = document.getElementById('password'); // textarea for password Generation
+let slider = document.getElementById('range'); // slider range 
+let output = document.getElementById('sliderValue'); // slider label
+let checkChar = document.getElementById('checkChar');
+let checkNumbers = document.getElementById('checkNumbers');
+let checkSpe = document.getElementById('checkSpe');
+let refresh = document.getElementById('refreshBtn');
 output.textContent = slider.value // sets default value
-
+let checkBoxs = document.getElementsByClassName('checkBox') // created to contain an HtmlCollection of all checkboxes 
 // Updates the slider value
 slider.oninput = function() {
     output.textContent = this.value
+}; 
+
+// Test to determine how to determine wether or not a checkbox is checked
+checkChar.oninput = function(){
+    if(checkChar.checked){
+        console.log("here")
+    }
 }
+
+
+function passwordGenerator() {
+
+
+    console.log(slider.value, checkChar.checked, checkNumbers.checked, checkSpe.checked)
+
+}
+
+slider.oninput( passwordGenerator )
+
+checkChar.addEventListener('click', passwordGenerator)
+
+
+
+
+// Have to create an array from the htmlCollection and forEach item in there we create an addeventlistener
+Array.from(checkBoxs).forEach((item) => {
+    item.addEventListener('click', passwordGenerator)
+})
+
+
+refresh.addEventListener('click', passwordGenerator);
