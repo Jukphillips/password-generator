f8396088b53a42c617ff64d17f568df6e5ff86f8
Justin Phillips <Jukphillips1990@gmail.com>
2022-09-20 19:46:44 -0600

    finshed dynamic css changing for psuedo elements

--- app.js
+++ app.js
@@ -9,35 +9,41 @@ const checkBoxs = document.getElementsByClassName('checkBox') // created to cont
 
 const card = document.getElementById('card')
 const password = document.getElementById('password')
-const passControls = document.getElementById('passwordControls')
+const passControls = document.getElementsByClassName('passwordControls')
 
 let lower = "abcdefghijklmnopqrstuvwxyz".split();
 let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split();
 let numeric = "1234567890".split();
 let special = "!@#$%^&*()_-[]{}\|;:',./?+=".split()
 
-
 output.textContent = slider.value // sets default value'
 
 function changeBackground(){
 if( slider.value < 7 ) {
-    card.style.backgroundColor = "rgb(226, 28, 28)";
-    password.style.backgroundColor = "rgb(226, 28, 28)";
-    passControls.style.backgroundColor = "rgb(226, 28, 28)";
-    passControls.style.border = "2px solid #cf1b1b"
+    card.style.backgroundColor = "#E21C1C";
+    password.style.backgroundColor = "#E21C1C";
+    passControls[0].style.backgroundColor = "#E21C1C";
+    passControls[0].style.border = "2px solid #cf1b1b";
+    passControls[0].classList.remove('middle', 'end');
+    passControls[0].classList.toggle('first')
     card.style.border = "4px solid #cf1b1b";
 } else if ( slider.value < 9 && slider.value >= 7 ){
     card.style.backgroundColor = "#f0ad4e";
     password.style.backgroundColor = "#f0ad4e";
-    passControls.style.backgroundColor = "#f0ad4e"
-    passControls.style.border = "2px solid #D29B47"
+    passControls[0].style.backgroundColor = "#f0ad4e";
+    passControls[0].style.border = "2px solid #D29B47";
+    passControls[0].classList.remove('first', 'end');
+    passControls[0].classList.add('middle')
     card.style.border = "4px solid #D29B47";
-} else {
+} else if (slider.value > 9){
     card.style.backgroundColor = "#5cb85c";
+    card.style.border = "4px solid #3DA13D";    
     password.style.backgroundColor = "#5cb85c";
-    passControls.style.backgroundColor = "#5cb85c";
-    passControls.style.border = "2px solid #3DA13D"
-    card.style.border = "4px solid #3DA13D";
+    passControls[0].style.backgroundColor = "#5cb85c";
+    passControls[0].style.border = "2px solid #3DA13D"
+    passControls[0].classList.remove('first', 'middle');
+    passControls[0].classList.add('end');
+
 }
 }
 
