7ccdf17b8c86cbe39fa69b756fb903a04e6c549c
Justin Phillips <Jukphillips1990@gmail.com>
2022-09-25 06:03:21 -0600

    added dynamic word coloring and fixed several bugs

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ const checkSpe = document.getElementById('checkSpe');
 const refresh = document.getElementById('refreshBtn');
 const checkBoxs = document.getElementsByClassName('checkBox') // created to contain an HtmlCollection of all checkboxes
 const copyPassBtn = document.getElementById('generate')
+const container = document.getElementsByClassName('passwordcontainer')
 
 // Section of dom elements for changing css
 const card = document.getElementById('card') //
-const password = document.getElementById('password')
+let password;
 const passControls = document.getElementsByClassName('passwordControls')
 
 // Section for character strings to determine password with
@@ -28,7 +29,7 @@ output.textContent = slider.value // sets default value'
 function changeBackground(){
 if( slider.value < 7 ) { // used to change css styling when slider value is under 7
     card.style.backgroundColor = "#E21C1C";
-    password.style.backgroundColor = "#E21C1C";
+    // containter.style.backgroundColor = "#E21C1C";
     passControls[0].style.backgroundColor = "#E21C1C";
     passControls[0].style.border = "2px solid #cf1b1b";
     passControls[0].classList.remove('middle', 'end');
@@ -36,7 +37,7 @@ if( slider.value < 7 ) { // used to change css styling when slider value is unde
     card.style.border = "4px solid #cf1b1b";
 } else if ( slider.value < 9 && slider.value >= 7 ){ // used to change slider value when values are in between 7 & 9
     card.style.backgroundColor = "#f0ad4e";
-    password.style.backgroundColor = "#f0ad4e";
+    // containter.style.backgroundColor = "#f0ad4e";
     passControls[0].style.backgroundColor = "#f0ad4e";
     passControls[0].style.border = "2px solid #D29B47";
     passControls[0].classList.remove('first', 'end');
@@ -45,7 +46,7 @@ if( slider.value < 7 ) { // used to change css styling when slider value is unde
 } else if (slider.value > 9){ // used to change css styling when slider value is above 9
     card.style.backgroundColor = "#5cb85c";
     card.style.border = "4px solid #3DA13D";    
-    password.style.backgroundColor = "#5cb85c";
+    // containter.style.backgroundColor = "#5cb85c";
     passControls[0].style.backgroundColor = "#5cb85c";
     passControls[0].style.border = "2px solid #3DA13D"
     passControls[0].classList.remove('first', 'middle');
@@ -57,6 +58,7 @@ if( slider.value < 7 ) { // used to change css styling when slider value is unde
 
 // Updates the slider value and calls writePassword function
 slider.oninput = function() {
+    
     output.textContent = this.value;
     changeBackground();
     writePassword(); 
@@ -64,16 +66,46 @@ slider.oninput = function() {
 
 // Places the password generated in passwordGenerator() into dom element to display password
 function writePassword() {
+    let childDivs = document.querySelectorAll(".passwordLetters")
+    console.log(childDivs)
+    for( let i = 0; i < childDivs.length; i++){
+        childDivs[i].remove();
+    }
+
     let output = passwordGenerator();
-    password.value = output;
+    let letter = new RegExp("[a-zA-Z]")
+    let digit = new RegExp("[^0-9]")
+    password = output;
+    console.log(password)
+    
+
+    for( var i = 0; i <= output.length; i++){
+        let newSpan = document.createElement('div')
+        newSpan.textContent = output.charAt(i);
+    
+        if(letter.test(output.charAt(i))){
+            newSpan.id = 'letters';
+        } else if (digit.test(output.charAt(i))){
+            newSpan.id = "digits";
+        } else {
+            newSpan.id = "specialChar";
+        }
+        
+        newSpan.classList.add("passwordLetters")
+        container[0].appendChild(newSpan)
+    }
+    
+    console.log(container[0].children.length)
 }
 
 function copyClip() {
-    navigator.clipboard.writeText(password.value) // connects to clipboard via navigator and writes new password to clipboard
+    console.log(password)
+    navigator.clipboard.writeText(password) // connects to clipboard via navigator and writes new password to clipboard
 }
    
 function passwordGenerator() {
     let output = '';
+    let numericChar = '';
     // function 
     let getRanChar = (str) => str.charAt(Math.floor(Math.random() * str.length));
     // requires users to include 
@@ -89,7 +121,9 @@ function passwordGenerator() {
     };
     if(checkNumbers.checked){
         userChoices+= numeric
-        output += getRanChar(numeric.toString())
+        numericChar += getRanChar(numeric.toString())
+        console.log(numericChar);
+        output += numericChar; 
     };
     if(checkSpe.checked){
         userChoices+= special
@@ -102,7 +136,7 @@ function passwordGenerator() {
 
 
     };
-    
+
     output = output.split('').sort(function(){return 0.5-Math.random()}).join('')
     return output
 
